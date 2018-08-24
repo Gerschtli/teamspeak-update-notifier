90561767f3f42c616ec5a276df0b143ad17b5349
Tobias Happ <tobias.happ@gmx.de>
2018-08-24 15:01:13 +0200

    Adds custom exceptions

---
+++ notifier/errors.py
@@ -0,0 +1,18 @@
+class Error(Exception):
+    exit_code = 127
+
+
+class ConnectionError(Error):
+    exit_code = 1
+
+
+class MessageError(Error):
+    exit_code = 2
+
+
+class ServerDisconnectError(Error):
+    exit_code = 3
+
+
+class SigTermError(Error):
+    exit_code = 4
