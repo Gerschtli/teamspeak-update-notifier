ad47e5c8cf786f02963680d3fa827d771c448241
Tobias Happ <tobias.happ@gmx.de>
2018-08-24 15:20:21 +0200

    Bumps version to 1.3.0

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup
 
-
 with open("README.md", "rb") as f:
     long_descr = f.read().decode("utf-8")
 
@@ -8,10 +7,12 @@ setup(
     name="teamspeak-update-notifier",
     packages=["notifier"],
     entry_points={
-        "console_scripts": ['teamspeak-update-notifier = notifier.notifier:main']
+        "console_scripts":
+        ['teamspeak-update-notifier = notifier.notifier:main']
     },
-    version="1.2.0",
-    description="Sends update notifications to server admins for teamspeak server.",
+    version="1.3.0",
+    description=("Sends update notifications to server admins for teamspeak "
+                 "server."),
     long_description=long_descr,
     author="Tobias Happ",
     author_email="tobias.happ@gmx.de",
