9541cb7a939ca9e1a652390248a143e09f3f23a3
Tobias Happ <tobias.happ@gmx.de>
2018-08-24 15:01:40 +0200

    Adds commands

---
+++ notifier/commands.py
@@ -0,0 +1,75 @@
+from .message import Message
+
+
+class CommandFactory:
+    def __init__(self, username, password, server_id):
+        self.username = username
+        self.password = password
+        self.server_id = server_id
+
+    def login(self):
+        return Login(self.username, self.password)
+
+    def notify_register(self):
+        return NotifyRegister()
+
+    def quit(self):
+        return Quit()
+
+    def send_message(self, client_id, message):
+        return SendMessage(client_id, message)
+
+    def use(self):
+        return Use(self.server_id)
+
+    def whoami(self):
+        return Whoami()
+
+
+class Login(Message):
+    def __init__(self, username, password):
+        super().__init__(
+            "login",
+            {
+                "client_login_name": username,
+                "client_login_password": password,
+            },
+        )
+
+
+class NotifyRegister(Message):
+    def __init__(self, ):
+        super().__init__(
+            "servernotifyregister",
+            {"event": "server"},
+        )
+
+
+class Quit(Message):
+    def __init__(self):
+        super().__init__("quit")
+
+
+class SendMessage(Message):
+    def __init__(self, client_id, message):
+        super().__init__(
+            "sendtextmessage",
+            {
+                "targetmode": "1",
+                "target": client_id,
+                "msg": message,
+            },
+        )
+
+
+class Use(Message):
+    def __init__(self, server_id):
+        super().__init__(
+            "use",
+            {"sid": server_id},
+        )
+
+
+class Whoami(Message):
+    def __init__(self):
+        super().__init__("whoami")
