74be416c9b71cd295e196bd9d5c9fbcbc6804a6f
Tobias Happ <tobias.happ@gmx.de>
2018-08-30 23:15:42 +0200

    Removes .pylintrc

--- lint.sh
+++ lint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-set -xe
+set -x
 
 pycodestyle .
 python -m pyflakes .
-find . -maxdepth 2 -type f -name "__init__.py" -exec dirname {} \; | xargs -I % pylint %
+find . -maxdepth 2 -type f -name "__init__.py" -exec dirname {} \; | \
+  xargs -I % pylint --extension-pkg-whitelist=dependency_injector --disable=missing-docstring,too-few-public-methods %
