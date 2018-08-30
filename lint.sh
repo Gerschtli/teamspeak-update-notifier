7892012d9b37bb4008490e88b9b05766c3aeba99
Tobias Happ <tobias.happ@gmx.de>
2018-08-30 23:22:53 +0200

    Show linter version

--- lint.sh
+++ lint.sh
@@ -2,6 +2,6 @@
 set -x
 
 pycodestyle .
-python -m pyflakes .
+pyflakes .
 find . -maxdepth 2 -type f -name "__init__.py" -exec dirname {} \; | \
   xargs -I % pylint --extension-pkg-whitelist=dependency_injector --disable=missing-docstring,too-few-public-methods %
