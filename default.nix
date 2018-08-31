076d37dae157144e9112b912b4a4f17afad39047
Tobias Happ <tobias.happ@gmx.de>
2018-08-31 17:34:08 +0200

    Use pypi for dependency-injector

--- default.nix
+++ default.nix
@@ -1,15 +1,13 @@
 with import <nixpkgs> { };
 
 let
-  dependency_injector = python36Packages.buildPythonPackage rec {
-    pname = "dependency_injector";
+  dependency-injector = python36Packages.buildPythonPackage rec {
+    pname = "dependency-injector";
     version = "3.13.1";
 
-    src = fetchFromGitHub {
-      owner = "ets-labs";
-      repo = pname;
-      rev = version;
-      sha256 = "03cn4aanz6q28vy5946bpbmnpg25szzp0q3m4wk82ardw5j6x07i";
+    src = python36Packages.fetchPypi {
+      inherit pname version;
+      sha256 = "0bmcgdfjavgxdzkb904q968ig1x44arvpj2m4rpm5nc9vhhgq43q";
     };
 
     propagatedBuildInputs = with python36Packages; [ six ];
@@ -24,7 +22,7 @@ python36.pkgs.buildPythonPackage rec {
 
   propagatedBuildInputs = with python36Packages; [
     beautifulsoup4
-    dependency_injector
+    dependency-injector
     requests
   ];
 }
