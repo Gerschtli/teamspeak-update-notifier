23f29c644c62cba0005d547da6a5019a719a1603
Tobias Happ <tobias.happ@gmx.de>
2018-12-28 01:50:56 +0100

    Update nix expressions

--- default.nix
+++ default.nix
@@ -1,19 +1,5 @@
 with import <nixpkgs> { };
 
-let
-  dependency-injector = python36Packages.buildPythonPackage rec {
-    pname = "dependency-injector";
-    version = "3.13.1";
-
-    src = python36Packages.fetchPypi {
-      inherit pname version;
-      sha256 = "0bmcgdfjavgxdzkb904q968ig1x44arvpj2m4rpm5nc9vhhgq43q";
-    };
-
-    propagatedBuildInputs = with python36Packages; [ six ];
-  };
-in
-
 python36.pkgs.buildPythonPackage rec {
   pname = "teamspeak-update-notifier";
   version = "HEAD";
--- shell.nix
+++ shell.nix
@@ -1,23 +1,15 @@
-let
-  pkgs = let
-    hostPkgs = import <nixpkgs> {};
-    pinnedPkgs = hostPkgs.fetchFromGitHub {
-      owner = "NixOS";
-      repo = "nixpkgs";
-      rev = "8f0bafcaff7744d3d489a4eed563eda76fc604f8";
-      sha256 = "163bs9pfal6w0h94fr6jsd9cb1p1axc13rbzrqh6wiajkrhjir5c";
-    };
-    in import pinnedPkgs { };
-
-  inherit (pkgs) python36Packages;
-in
+with import <nixpkgs> { };
 
 (import ./.).overrideDerivation (old: {
   name = old.pname;
 
   buildInputs = old.buildInputs
     ++ (with python36Packages; [
-      pycodestyle pyflakes pylint python-language-server mypy
+      mypy
+      pycodestyle
+      pyflakes
+      pylint
+      python-language-server
     ]);
 
   PYTHONDONTWRITEBYTECODE = 1;
