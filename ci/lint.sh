9639bf57e69747c2affe0a39dca63af5e423cdd5
Tobias Happ <tobias.happ@gmx.de>
2018-12-30 00:46:25 +0100

    Update linter configs

--- ci/lint.sh
+++ ci/lint.sh
@@ -3,6 +3,6 @@ set -xe
 
 pycodestyle .
 pyflakes .
-pylint --extension-pkg-whitelist=dependency_injector --disable=missing-docstring,too-few-public-methods notifier
-pylint --extension-pkg-whitelist=dependency_injector --disable=missing-docstring,too-few-public-methods,invalid-name tests
-mypy notifier tests --ignore-missing-imports
+pylint --disable=missing-docstring,too-few-public-methods notifier
+pylint --disable=missing-docstring,too-few-public-methods,invalid-name tests
+mypy notifier tests
