5b03298487cf6497e23574c63b8f81f7bf77bd33
Tobias Happ <tobias.happ@gmx.de>
2019-01-01 18:22:45 +0100

    Bump version to 1.4.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name="teamspeak-update-notifier",
     packages=["notifier"],
     entry_points={"console_scripts": ["teamspeak-update-notifier = notifier.__main__:main"]},
-    version="1.4.1",
+    version="1.4.2",
     description="Sends update notifications to server admins for teamspeak server.",
     long_description=long_descr,
     author="Tobias Happ",
