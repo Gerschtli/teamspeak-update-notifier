579cda2fd3a187bc2d6fd24a07dc68132f2e7e8d
Tobias Happ <tobias.happ@gmx.de>
2020-11-29 14:56:48 +0100

    Send keep alive messages

--- notifier/client.py
+++ notifier/client.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import queue
+import time
 from types import TracebackType
 from typing import List, Optional, Type, TypeVar
 
@@ -13,9 +14,11 @@ class Client:
     def __init__(self, queue_read: queue.Queue, queue_write: queue.Queue) -> None:
         self._queue_read = queue_read
         self._queue_write = queue_write
+        self._last_message_sent = 0
 
     def execute(self, command: commands.Command) -> Optional[T]:
         self._queue_write.put(command.message)
+        self._last_message_sent = time.time()
 
         result = None
         if isinstance(command, commands.QueryCommand):
@@ -26,8 +29,18 @@ class Client:
         return result
 
     def listen(self, handlers_list: List[handlers.Handler]) -> None:
+        keep_alive = commands.KeepAlive()
+
         while True:
-            message = self._queue_read.get()
+            # ts3 server expects a message at least every 5 min, so to be sure send every 4 min
+            if time.time() - self._last_message_sent > 4 * 60:
+                self.execute(keep_alive)
+
+            try:
+                message = self._queue_read.get(timeout=30)
+            except queue.Empty:
+                continue
+
             if message is None:
                 continue
 
