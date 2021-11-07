ec9ae85ec46aef3e1dafb3e75421a9f674041d4e
Tobias Happ <tobias.happ@gmx.de>
2021-11-08 00:22:44 +0100

    Bump version to 1.7.1

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 
 pkgs.python39.pkgs.buildPythonPackage rec {
   pname = "teamspeak-update-notifier";
-  version = "1.7.0";
+  version = "1.7.1";
 
   src = builtins.filterSource
     (path: type: type != "directory" || baseNameOf path != "teamspeak_update_notifier.egg-info")
