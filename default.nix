f6ae282ddc66058f3799fd84146d743b908c57ac
Tobias Happ <tobias.happ@gmx.de>
2025-12-27 14:28:44 +0100

    fix: update to support latest nixpkgs

--- default.nix
+++ default.nix
@@ -8,6 +8,11 @@ pkgs.python3.pkgs.buildPythonPackage rec {
     (path: type: type != "directory" || baseNameOf path != "teamspeak_update_notifier.egg-info")
     ./.;
 
+  pyproject = true;
+  build-system = with pkgs.python3Packages; [
+    setuptools
+  ];
+
   propagatedBuildInputs = with pkgs.python3Packages; [
     beautifulsoup4
     requests
